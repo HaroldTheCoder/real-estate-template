49affd3925d025e3e4706c3b86c636f1a192e03d
Jarol Riera <80286893+HaroldTheCoder@users.noreply.github.com>
2022-04-25 11:39:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const navbar = document.querySelector('#main-navbar');
 const navbarToggler = document.querySelector('.navbar-toggler');
+const counters = document.querySelectorAll('.value');
+const speed = 200;
+let statsBox = document.querySelector('#section-7').offsetTop - 300;
+
 
 // Scroll functions 
 window.addEventListener('scroll', ()=> {
+    /* Fixed Navbar function */
     if(window.scrollY > 0) {
         navbar.classList.add('active-scroll');
         navbar.classList.add('shadow');
@@ -10,6 +15,26 @@ window.addEventListener('scroll', ()=> {
         navbar.classList.remove('active-scroll');
         navbar.classList.remove('shadow');
     }
+
+    /* Stats Counter function - triggers on if elements position equals viewport*/
+    if(window.scrollY >= statsBox ) {
+        counters.forEach( counter => {
+            const animate = () => {
+                const value = +counter.getAttribute('data-value');
+                const data = +counter.innerText;
+     
+                const time = value / speed;
+                if(data < value) {
+                    counter.innerText = Math.ceil(data + time);
+                    setTimeout(animate, 1);
+                } else{
+                    counter.innerText = value;
+                }
+            }
+            animate();
+        });
+    }
+
 });
 
 // Navbar Toggler custom function
@@ -17,24 +42,8 @@ navbarToggler.addEventListener('click', ()=> {
     navbar.classList.toggle('open');
 })
 
-// Stats Counter function
-const counters = document.querySelectorAll('.value');
-const speed = 200;
 
-counters.forEach( counter => {
-   const animate = () => {
-      const value = +counter.getAttribute('data-value');
-      const data = +counter.innerText;
-     
-      const time = value / speed;
-     if(data < value) {
-          counter.innerText = Math.ceil(data + time);
-          setTimeout(animate, 1);
-        }else{
-          counter.innerText = value;
-        }
-     
-   }
-   
-   animate();
-});
+
+
+
+
